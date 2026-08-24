9c13d6e121ed35258a56268c68ed9a43e922af89
Shivam Kalekar <154619763+ShivamK-45@users.noreply.github.com>
2026-08-24 22:22:40 +0530

    Analytic service completed

--- server/src/server.js
+++ server/src/server.js
@@ -21,6 +21,7 @@ import cookieParser from "cookie-parser"
 import authRouter from "./services/auth/routes/authRouter.js";
 import clientRouter from "./services/client/routes/clientRoutes.js";
 import ingestRouter from "./services/ingest/routes/ingestRoutes.js";
+import analyticsRouter from "./services/analytics/routes/analyticsRoutes.js";
 
 /**
  * Initialize Express app
@@ -95,8 +96,8 @@ app.get("/", (req, res) => {
  */
 app.use("/api/auth", authRouter);
 app.use("/api/hit", ingestRouter);
+app.use("/api/analytics", analyticsRouter);
 app.use("/api", clientRouter);
-
 
 /**
  * 404 Handler
